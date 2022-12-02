6905d72c2aa6572220ad4535ed5389072f400ca8
Marc Rosenthal <marc.rosenthal@jpmchase.com>
2022-12-02 12:35:28 -0600

    small cleanup

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -10,19 +10,21 @@ fn main() {
     println!("Day 02!");
 
     let score_shape: HashMap<&str,i32> = HashMap::from([
-        ("X", 1),
-        ("Y", 2),
-        ("Z", 3),
         ("rock", 1),
         ("paper", 2),
         ("scissor", 3)
     ]);
 
-
     let score_outcome: HashMap<&str,i32> = HashMap::from([
         ("win", 6),
         ("lose", 0),
         ("draw", 3)
+    ]);
+    
+    let shape_lookup: HashMap<&str,&str> = HashMap::from([
+        ("X", "rock"),
+        ("Y", "paper"),
+        ("Z", "scissor")
     ]);
 
     let score_outcome_2: HashMap<&str,&str> = HashMap::from([
@@ -66,14 +68,15 @@ fn main() {
             let line_str: &str = &line.unwrap().parse::<String>().unwrap();
             let line_arr: Vec<&str> = line_str.split(" ").collect();
 
-            let match_outcome_str: &str = &matchup_outcome.get(line_str).unwrap();
-            score += score_shape.get(line_arr[1]).unwrap() + score_outcome.get(match_outcome_str).unwrap();
+            score += 
+                score_shape.get(shape_lookup.get(line_arr[1]).unwrap()).unwrap() + 
+                score_outcome.get(matchup_outcome.get(line_str).unwrap()).unwrap();
 
-            let score_outcome_2_str: &str = &score_outcome_2.get(line_arr[1]).unwrap();
-            let play2key = score_outcome_2_str.to_owned() + line_arr[0];
-            let play2_match_outcome: &str = &matchup_outcome_play.get(&*play2key).unwrap();
-            let line_score: i32 = score_outcome.get(score_outcome_2_str).unwrap() + *score_shape.get(play2_match_outcome).unwrap();
-            score_2 += line_score;
+            let play2_outcome: &str = &score_outcome_2.get(line_arr[1]).unwrap();
+            let play2_key: &str = &(play2_outcome.to_owned() + line_arr[0]);
+            score_2 += 
+                score_outcome.get(play2_outcome).unwrap() + 
+                *score_shape.get(matchup_outcome_play.get(play2_key).unwrap()).unwrap();
         }
         println!("-----------------------------");
         println!("score = {:?}", score);
@@ -86,3 +89,7 @@ where P: AsRef<Path>, {
     let file = File::open(filename)?;
     Ok(io::BufReader::new(file).lines())
 }
+
+// -----------------------------
+// score = 14163
+// score_2 = 12091
